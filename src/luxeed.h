644b9bea1f1045e8c1944e81c03ca4845c599ece
Kurt Stephens <kurt@trans.dev.cashnetusa.com>
2008-12-10 05:25:27 -0600

    Working driver; sometimes it errors out.

--- src/luxeed.h
+++ src/luxeed.h
@@ -16,17 +16,41 @@ typedef struct luxeed_device {
   struct usb_device *u_dev;
   usb_dev_handle *u_dh;
 
-  unsigned char key_data[64 * 3];
+#define LUXEED_NUM_OF_KEYS 76
+  /* RGB order, 0x00 - 0xff scale. */
+  unsigned char key_data[LUXEED_NUM_OF_KEYS * 3];
 
-  unsigned char msg[0x180];
+  /* Message buffer */
+  unsigned char *msg;
+  int msg_len;
   int msg_size;
+
+  int debug;
 } luxeed_device;
+
+typedef struct luxeed_key {
+  int id;
+  const char *name[3];
+  int code[3];
+} luxeed_key;
 
 luxeed_device *luxeed_find_device(uint16_t vendor, uint16_t product);
 int luxeed_open(luxeed_device *dev);
 int luxeed_close(luxeed_device *dev);
+int luxeed_destroy(luxeed_device *dev);
 int luxeed_init(luxeed_device *dev);
 int luxeed_send(luxeed_device *dev, int ep, unsigned char *bytes, int size);
 int luxeed_update(luxeed_device *dev);
+
+luxeed_key *luxeed_key_id(luxeed_device *dev, int i);
+luxeed_key *luxeed_key_name(luxeed_device *dev, const char *keyname);
+luxeed_key *luxeed_key_ascii(luxeed_device *dev, int c);
+
+#define LUXEED_COLOR_MIN 0x00
+#define LUXEED_COLOR_MAX 0xff
+
+unsigned char *luxeed_pixel(luxeed_device *dev, int key);
+int luxeed_set_key_color(luxeed_device *dev, luxeed_key *key, int r, int g, int b);
+int luxeed_key_color(luxeed_device *dev, luxeed_key *key, int *r, int *g, int *b);
 
 #endif
---
+++ src/luxeed_test.c
@@ -0,0 +1,99 @@
+
+#include <stdio.h>
+#include <stdlib.h> /* memset(), memcpy() */
+#include <string.h>
+#include <assert.h>
+#include "luxeed.h"
+
+
+static char *progname;
+int main (int argc, char **argv)
+{
+  int result = 0;
+  luxeed_device *dev;
+
+  progname = argv[0];
+  
+  dev = luxeed_find_device(0, 0);
+
+  if ( ! dev ) {
+    fprintf(stderr, "%s: luxeed keyboard not found\n", progname);
+    return 1;
+  }
+
+  // dev->debug ++;
+
+  {
+    int msg_id = -1;
+
+    srand(getpid() ^ time(0));
+
+    while ( 1 ) {
+      int i = (++ msg_id) % LUXEED_NUM_OF_KEYS;
+      int j;
+      float scale, scale_dir;
+      int r, g, b;
+      float max;
+
+      do { 
+	r = rand() & 0xff;
+	g = rand() & 0xff;
+	b = rand() & 0xff;
+	
+	max = r;
+	if ( max < g ) max = g;
+	if ( max < b ) max = b;
+
+	if ( max != 0 ) {
+	  r = LUXEED_COLOR_MAX * (r / max);
+	  g = LUXEED_COLOR_MAX * (g / max);
+	  b = LUXEED_COLOR_MAX * (b / max);
+	}
+      } while ( max == 0 );
+      // fprintf(stderr, "%02x %02x %02x\n", r, g, b);
+
+      for ( scale = 0, scale_dir = 0.1; ! (scale <= 0 && scale_dir < 0); ) {
+	int r1, g1, b1;
+	r1 = r * scale;
+	g1 = g * scale;
+        b1 = b * scale;
+	// fprintf(stderr, "%02x %02x %02x\n", r1, g1, b1);
+
+	memset(dev->key_data, 0, sizeof(dev->key_data));
+
+	for ( j = 0; j < 10; ++ j ) {
+	  int k = (i + j) % LUXEED_NUM_OF_KEYS;
+	  unsigned char *pixel = luxeed_pixel(dev, k);
+	  pixel[0] = r1;
+	  pixel[1] = g1;
+	  pixel[2] = b1;
+	}
+
+	result = luxeed_update(dev);
+	if ( result ) goto done;
+	usleep(50000);
+
+	scale += scale_dir;
+	if ( scale > 1.0 ) {
+	  scale_dir = - scale_dir;
+	  scale += scale_dir;
+	}
+
+	fprintf(stderr, scale_dir < 0 ? "<" : ">");
+	fflush(stderr);
+      }
+    }
+  }
+
+ done:
+  if ( result ) {
+    fprintf(stderr, "error: %d\n", result);
+  }
+  luxeed_destroy(dev);
+  
+  return 0;
+}
+
+
+/* EOF */
+
