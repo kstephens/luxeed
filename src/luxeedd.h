401adaf276c036a7ff62bf284fafe139923d99c1
Kurt Stephens <kurt@trans.dev.cashnetusa.com>
2008-12-14 20:13:12 -0600

    FIFO daemon using libevent.

--- src/luxeedd.h
+++ src/luxeedd.h
@@ -5,18 +5,51 @@
 #include <stdlib.h> /* memset(), memcpy() */
 #include <string.h>
 #include <assert.h>
+#include <netinet/in.h>
+#include <sys/un.h>
+#include <sys/socket.h>
 
 #include "luxeed.h"
 #include "event.h"
 
 
-typedef struct luxeedd_client {
+typedef struct luxeedd_server {
+  const char *progname;
+
   luxeed_device *dev;
+
+  char server_uri[1024];
+  char server_path[1024];
+
+  int server_fd;
+  struct sockaddr_un socket_addr;
+  socklen_t socket_addr_size;
+
+  int options;
+  int debug;
+
+  struct event accept_ev;
+
+} luxeedd_server;
+
+int luxeedd_server_open(luxeedd_server *srv);
+int luxeedd_server_close(luxeedd_server *srv);
+
+void luxeedd_server_accept(int fd, short event, void *data);
+
+
+typedef struct luxeedd_client {
+  luxeedd_server *srv;
 
   int in_fd, out_fd;
   FILE *in, *out;
 
+  int socket_fd;
+  struct sockaddr_un socket_addr;
+  socklen_t socket_addr_size;
+
   int options;
+  int debug;
 
   int color[3];
 
@@ -24,7 +57,13 @@ typedef struct luxeedd_client {
 
 } luxeedd_client;
 
+
+int luxeedd_client_open(luxeedd_client *cli, luxeedd_server *srv, int in_fd, int out_fd);
+int luxeedd_client_close(luxeedd_client *cli);
+
 /* Returns -1 on EOF */
-int luxeed_read_command(luxeed_client *cli);
+int luxeed_read_command(luxeedd_client *cli);
+
+void luxeedd_client_read(int fd, short event, void *data);
 
 #endif
